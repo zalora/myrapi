d07211808f1bf8902323416d36415307c88b32b3
Vladimir Kirillov <proger@wilab.org.ua>
2014-10-06 23:20:58 +0300

    skel

--- Myracloud.hs
+++ Myracloud.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Myracloud where
 
@@ -12,19 +14,19 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as B8
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Base64 as Base64
-import Data.Byteable (toBytes)
 import Data.Default
 
 import Data.Aeson (Value)
 import qualified Data.Aeson as A
 import qualified Data.Aeson.Parser as A
+import qualified Data.Aeson.Encode.Pretty as A
 
 import Data.Conduit (($$+-))
 import qualified Data.Conduit as C
-import qualified Data.Conduit.List as CL
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Trans.Resource (ResourceT, MonadThrow (throwM))
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Resource (ResourceT, MonadThrow (throwM), runResourceT)
 import qualified Data.Conduit.Attoparsec as CA
 
 import qualified Network.HTTP.Types as HTTP
@@ -36,26 +38,26 @@ import Data.Time.Format (formatTime)
 
 import Crypto.Hash (digestToHexByteString, hmacGetDigest, hash, hmac, HMAC, Digest, MD5, SHA512, SHA256)
 
+import System.Posix.Env.ByteString (getEnv)
+
 type HTTPResponseConsumer a = HTTP.Response (C.ResumableSource (ResourceT IO) ByteString)
                               -> ResourceT IO a
 
 consumeValue :: HTTPResponseConsumer Value
 consumeValue res = HTTP.responseBody res $$+- CA.sinkParser A.json
 
-class MyraHttp a where
-  myraToHttp :: a -> HTTP.Request
-  myraToHttp _ =
-      def { HTTP.method = HTTP.methodGet
-          , HTTP.secure = True
-          , HTTP.host = "api.myracloud.com"
-          , HTTP.port = 443
-          , HTTP.path = "/en/rapi/DNSRecords/example.com/1"
-          , HTTP.queryString = mempty
-          , HTTP.requestHeaders = []
-          , HTTP.requestBody = mempty
-          , HTTP.decompress = HTTP.alwaysDecompress
-          , HTTP.checkStatus = \_ _ _ -> Nothing
-          }
+defHttp =
+    def { HTTP.method = HTTP.methodGet
+        , HTTP.secure = True
+        , HTTP.host = "api.myracloud.com"
+        , HTTP.port = 443
+        , HTTP.queryString = mempty
+        , HTTP.requestHeaders = [("Content-Type", "application/json")]
+        , HTTP.requestBody = mempty
+        , HTTP.decompress = HTTP.alwaysDecompress
+        }
+
+class MyraHttp a where myraToHttp :: a -> HTTP.Request
 
 class MyraHttp req => MyraCall req resp where
   consume :: req -> HTTPResponseConsumer resp
@@ -64,41 +66,73 @@ class MyraHttp req => MyraCall req resp where
 
 data ListDNSRecords = ListDNSRecords ByteString
 
-instance MyraHttp ListDNSRecords
+instance MyraHttp ListDNSRecords where
+  myraToHttp (ListDNSRecords domain) = defHttp { HTTP.path = mconcat ["/en/rapi/DNSRecords/", domain] }
+
 instance MyraCall ListDNSRecords Value
 
 iso8601 :: UTCTime -> ByteString
 iso8601 = B8.pack . formatTime defaultTimeLocale "%FT%T%QZ"
 
-myra :: MyraCall req resp => HTTP.Manager -> req -> IO resp
-myra mgr req = do
-    now <- getCurrentTime
-    let iso = iso8601 now
-        ht = myraToHttp req
-        ht' = ht { HTTP.requestHeaders = ("Date", iso):(HTTP.requestHeaders ht) }
-  
-    undefined
-
 data MyraSignature = MyraSignature { myra_rqBody :: Maybe ByteString
                                    , myra_method :: HTTP.Method
                                    , myra_uri :: ByteString
                                    , myra_contentType :: ByteString
-                                   , myra_date :: ByteString -- "2014-04-26CET13:04:00+0100"
-                                   }
+                                   , myra_date :: ByteString
+                                   } deriving (Show)
 
 hmacHex = digestToHexByteString . hmacGetDigest
 md5 = digestToHexByteString . (hash :: ByteString -> Digest MD5)
 
 myraSignature :: ByteString -> ByteString -> MyraSignature -> ByteString
-myraSignature apiKey secret MyraSignature{..} = mconcat [ apiKey, ":", b64signature ]
+myraSignature apiKey secret MyraSignature{..} = mconcat [ "MYRA ", apiKey, ":", b64signature ]
   where
-    b64signature = Base64.encode $ toBytes signature
-    signature = hmac (toBytes signingKey) signingString :: HMAC SHA512
-
-    key = mconcat [ "MYRA", secret ]
-    dateKey = hmac key myra_date :: HMAC SHA256
-    signingKey = hmac (toBytes dateKey) "myra-api-request" :: HMAC SHA256
-
     md5body = md5 $ maybe "" id myra_rqBody
     signingString = BS.intercalate "#" [ md5body, myra_method, myra_uri, myra_contentType, myra_date ]
+
+    dateKey = hmac (mconcat [ "MYRA", secret ]) myra_date :: HMAC SHA256
+    signingKey = hmac (hmacHex dateKey) "myra-api-request" :: HMAC SHA256
+
+    signature = hmac (hmacHex signingKey) signingString :: HMAC SHA512
+    b64signature = Base64.encode $ hmacHex signature
+
+myra :: MyraCall req resp => HTTP.Manager -> req -> ResourceT IO resp
+myra mgr req = do
+    now <- liftIO $ getCurrentTime
+    Just access <- liftIO $ getEnv "MYRA_ACCESS_KEY"
+    Just secret <- liftIO $ getEnv "MYRA_SECRET_KEY"
+    let iso = iso8601 now
+        ht = myraToHttp req
+        sigData = MyraSignature { myra_rqBody = mempty
+                                , myra_method = HTTP.method ht
+                                , myra_uri = HTTP.path ht
+                                , myra_contentType = maybe (error "no content-type") id
+                                                     (lookup "Content-Type" $ HTTP.requestHeaders ht)
+                                , myra_date = iso
+                                }
+        sig = myraSignature access secret sigData
+        ht' = ht { HTTP.requestHeaders = ("Date", iso):("Authorize", sig):(HTTP.requestHeaders ht) }
+
+    liftIO . print $ HTTP.requestHeaders ht'
+    liftIO . print $ sigData
+
+    hresp <- HTTP.http ht' mgr
+    consume req hresp
+
+myra_ :: MyraCall req Value => req -> IO ()
+myra_ req = do
+  HTTP.withManager $ \mgr -> do
+    value :: Value <- myra mgr req
+    liftIO $ L8.putStrLn $ A.encodePretty value
+
+trySign = do
+    Just access <- getEnv "MYRA_ACCESS_KEY"
+    Just secret <- getEnv "MYRA_SECRET_KEY"
+    let date = "2014-10-06T19:37:54.174591Z"
+    print $ myraSignature access secret $ MyraSignature { myra_rqBody = Nothing
+                                                        , myra_method = "GET"
+                                                        , myra_uri = "/en/rapi/DNSRecords/zalora.sg"
+                                                        , myra_contentType = "application/json"
+                                                        , myra_date = date
+                                                        }
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-### dragons ahead!
+### Haskell client for Myracloud API 
+
+Work in progress.
+
+```console
+export MYRA_ACCESS_KEY=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+export MYRA_SECRET_KEY=bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+
+ghci -XOverloadedStrings Myracloud.hs
+> myra_ $ ListDNSRecords "example.com"
+```
