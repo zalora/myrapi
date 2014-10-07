5a09cadeb6170e2e4fbcbbb79dd3fa406e3d8d3a
Vladimir Kirillov <proger@wilab.org.ua>
2014-10-07 19:22:01 +0100

    cleanups, moving code around; while here add the page argument to myraToHttp

--- Myracloud.hs
+++ Myracloud.hs
@@ -15,7 +15,6 @@ import qualified Data.ByteString.Char8 as B8
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as L8
-import qualified Data.ByteString.Base64 as Base64
 import Data.Default
 
 import Data.Aeson (Value, FromJSON, parseJSON)
@@ -27,7 +26,7 @@ import qualified Data.Aeson.Encode.Pretty as A
 import Data.Conduit (($$+-))
 import qualified Data.Conduit as C
 import Control.Monad.IO.Class
-import Control.Monad.Trans.Resource (ResourceT, MonadThrow (throwM), runResourceT)
+import Control.Monad.Trans.Resource (ResourceT, MonadThrow (throwM), runResourceT, MonadResource)
 import qualified Data.Conduit.Attoparsec as CA
 
 import qualified Network.HTTP.Types as HTTP
@@ -37,15 +36,13 @@ import Data.Time.Clock
 import System.Locale (defaultTimeLocale)
 import Data.Time.Format (formatTime)
 
-import Crypto.Hash (digestToHexByteString, hmacGetDigest, hash, hmac, HMAC, Digest, MD5, SHA512, SHA256)
-import Data.Byteable (toBytes)
-
 import System.Posix.Env.ByteString (getEnv)
 
 import qualified Myracloud.Types as MT
+import Myracloud.Signature
 
-type HTTPResponseConsumer a = HTTP.Response (C.ResumableSource (ResourceT IO) ByteString)
-                              -> ResourceT IO a
+type HTTPResponse m = HTTP.Response (C.ResumableSource m ByteString)
+type HTTPResponseConsumer a = HTTPResponse (ResourceT IO) -> ResourceT IO a
 
 consumeValue :: HTTPResponseConsumer Value
 consumeValue res = HTTP.responseBody res $$+- CA.sinkParser A.json
@@ -61,7 +58,9 @@ defHttp =
         , HTTP.decompress = HTTP.alwaysDecompress
         }
 
-class MyraHttp a where myraToHttp :: a -> HTTP.Request
+type Page = Maybe Int
+
+class MyraHttp a where myraToHttp :: a -> Page -> HTTP.Request
 
 class MyraHttp req => MyraCall req resp where
   consume :: req -> HTTPResponseConsumer resp
@@ -71,42 +70,24 @@ class MyraHttp req => MyraCall req resp where
 data ListDNSRecords = ListDNSRecords ByteString
 
 instance MyraHttp ListDNSRecords where
-  myraToHttp (ListDNSRecords domain) = defHttp { HTTP.path = mconcat ["/en/rapi/dnsRecords/", domain ] }
+  myraToHttp (ListDNSRecords domain) page =
+    defHttp {
+      HTTP.path = mconcat [
+        "/en/rapi/dnsRecords/", domain, "/", B8.pack $ maybe "1" show page
+        ]
+      }
 
 instance MyraCall ListDNSRecords Value
 
 iso8601 :: UTCTime -> ByteString
 iso8601 = B8.pack . formatTime defaultTimeLocale "%FT%T%QZ"
 
-data MyraSignature = MyraSignature { myra_rqBody :: Maybe ByteString
-                                   , myra_method :: HTTP.Method
-                                   , myra_uri :: ByteString
-                                   , myra_contentType :: ByteString
-                                   , myra_date :: ByteString
-                                   } deriving (Show)
-
-hmacHex = digestToHexByteString . hmacGetDigest
-md5 = digestToHexByteString . (hash :: ByteString -> Digest MD5)
-
-myraSignature :: ByteString -> ByteString -> MyraSignature -> ByteString
-myraSignature apiKey secret MyraSignature{..} = mconcat [ "MYRA ", apiKey, ":", b64signature ]
-  where
-    md5body = md5 $ maybe "" id myra_rqBody
-    signingString = BS.intercalate "#" [ md5body, myra_method, myra_uri, myra_contentType, myra_date ]
-
-    dateKey = hmac (mconcat [ "MYRA", secret ]) myra_date :: HMAC SHA256
-    signingKey = hmac (hmacHex dateKey) "myra-api-request" :: HMAC SHA256
-
-    signature = hmac (hmacHex signingKey) signingString :: HMAC SHA512
-    b64signature = Base64.encode $ toBytes . hmacGetDigest $ signature
-
-myra :: MyraCall req resp => HTTP.Manager -> req -> ResourceT IO resp
-myra mgr req = do
+myra1 :: MonadResource m => HTTP.Request -> HTTP.Manager -> m (HTTPResponse m)
+myra1 ht mgr = do
     now <- liftIO $ getCurrentTime
     Just access <- liftIO $ getEnv "MYRA_ACCESS_KEY"
     Just secret <- liftIO $ getEnv "MYRA_SECRET_KEY"
     let iso = iso8601 now
-        ht = myraToHttp req
         sigData = MyraSignature { myra_rqBody = mempty
                                 , myra_method = HTTP.method ht
                                 , myra_uri = HTTP.path ht
@@ -120,17 +101,23 @@ myra mgr req = do
     liftIO . print $ HTTP.requestHeaders ht'
     liftIO . print $ sigData
 
-    hresp <- HTTP.http ht' mgr
-    consume req hresp
+    HTTP.http ht' mgr
 
-castValue :: FromJSON a => Value -> Maybe a
-castValue v = parseMaybe (const (parseJSON v)) v
+myra :: MyraCall req resp => HTTP.Manager -> req -> ResourceT IO resp
+myra mgr req = do
+    let ht = myraToHttp req Nothing
+    hresp <- myra1 ht mgr
+    consume req hresp
 
 myra_ :: MyraCall req Value => req -> IO ()
 myra_ req = do
   HTTP.withManager $ \mgr -> do
     value :: Value <- myra mgr req
     liftIO $ print $ (castValue value :: Maybe (MT.ObjectVO MT.DnsRecord))
+  where
+    castValue :: FromJSON a => Value -> Maybe a
+    castValue v = parseMaybe (const (parseJSON v)) v
+
 
 trySign = do
     Just access <- getEnv "MYRA_ACCESS_KEY"
---
+++ Myracloud/Signature.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Myracloud.Signature where
+
+import Data.Monoid (mconcat, mempty)
+
+import qualified Data.ByteString as BS
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Base64 as Base64
+
+import Crypto.Hash (digestToHexByteString, hmacGetDigest, hash, hmac, HMAC, Digest, MD5, SHA512, SHA256)
+import Data.Byteable (toBytes)
+
+import qualified Network.HTTP.Types as HTTP
+
+data MyraSignature = MyraSignature { myra_rqBody :: Maybe ByteString
+                                   , myra_method :: HTTP.Method
+                                   , myra_uri :: ByteString
+                                   , myra_contentType :: ByteString
+                                   , myra_date :: ByteString
+                                   } deriving (Show)
+
+hmacHex = digestToHexByteString . hmacGetDigest
+md5 = digestToHexByteString . (hash :: ByteString -> Digest MD5)
+
+myraSignature :: ByteString -> ByteString -> MyraSignature -> ByteString
+myraSignature apiKey secret MyraSignature{..} = mconcat [ "MYRA ", apiKey, ":", b64signature ]
+  where
+    md5body = md5 $ maybe "" id myra_rqBody
+    signingString = BS.intercalate "#" [ md5body, myra_method, myra_uri, myra_contentType, myra_date ]
+
+    dateKey = hmac (mconcat [ "MYRA", secret ]) myra_date :: HMAC SHA256
+    signingKey = hmac (hmacHex dateKey) "myra-api-request" :: HMAC SHA256
+
+    signature = hmac (hmacHex signingKey) signingString :: HMAC SHA512
+    b64signature = Base64.encode $ toBytes . hmacGetDigest $ signature
